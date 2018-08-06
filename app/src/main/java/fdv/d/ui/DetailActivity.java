26cce1a7cb281a4e6d1bfba21854c305ced24a9b
Nick <fabdadv@gmai.com>
2018-08-06 11:19:50 +0700

    Collect Ingredients

--- app/src/main/java/fdv/d/ui/DetailActivity.java
+++ app/src/main/java/fdv/d/ui/DetailActivity.java
@@ -3,7 +3,6 @@ package fdv.d.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -17,15 +16,16 @@ import com.squareup.picasso.Picasso;
 
 import fdv.d.R;
 import fdv.d.App;
-import fdv.d.data.api.DrinksList;
+import fdv.d.utils.Utils;
 import fdv.d.data.db.Drink;
+import fdv.d.data.api.DrinksList;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_ID_DRINK = "id_drink";
     public static final String EXTRA_PATH = "path_drink";
-    private String text;
     private TextView tvDrink;
     private TextView tvCategory;
+    private TextView tvIngredients;
     private TextView tvInstruction;
 
     @Override
@@ -37,8 +37,9 @@ public class DetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+/*              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+*/
                 Intent updateIntent = new Intent(DetailActivity.this, UpdateActivity.class);
                 startActivity(updateIntent);
 
@@ -47,9 +48,9 @@ public class DetailActivity extends AppCompatActivity {
 
         ImageView drinkView = findViewById(R.id.iv_drink);
         drinkView.setMaxWidth(100);
-        TextView tvIdDrink = findViewById(R.id.tv_id_drink);
         tvDrink = findViewById(R.id.tv_drink);
         tvCategory = findViewById(R.id.tv_cat);
+        tvIngredients = findViewById(R.id.tv_ings);
         tvInstruction = findViewById(R.id.tv_text);
 
         String id_drink = getIntent().getStringExtra(EXTRA_ID_DRINK);
@@ -61,14 +62,9 @@ public class DetailActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     Log.d("API_ID"," is Ok");
                     Drink drink = response.body().getList().get(0);
-                    Log.d("API_ID"," 1" + drink.getStrIngredient1() + drink.getStrMeasure1());
-                    Log.d("API_ID"," 2" + drink.getStrIngredient2() + drink.getStrMeasure2());
-                    Log.d("API_ID"," 3" + drink.getStrIngredient3() + drink.getStrMeasure3());
-                    Log.d("API_ID"," 4" + drink.getStrIngredient4() + drink.getStrMeasure4());
-                    Log.d("API_ID"," 5" + drink.getStrIngredient5() + drink.getStrMeasure5());
-                    text = drink.getStrInstructions();
-                    Log.d("API+ID", " I " + text);
-
+                    String s = Utils.getIngregientsList(drink);
+                    Log.d("API_ID"," Ings: " +s);
+                    tvIngredients.setText(s);
                     tvDrink.setText(drink.getName());
                     tvCategory.setText(drink.getStrCategory());
                     tvInstruction.setText(drink.getStrInstructions());
@@ -81,7 +77,6 @@ public class DetailActivity extends AppCompatActivity {
                 Log.e("API_ID", "Error: " + t.toString());
             }
         });
-        tvIdDrink.setText(id_drink);
         Picasso.get()
                 .load(path_drink)
                 .placeholder(R.drawable.no_drink)
--- app/src/main/java/fdv/d/utils/Utils.java
+++ app/src/main/java/fdv/d/utils/Utils.java
@@ -1,11 +1,42 @@
 package fdv.d.utils;
 
+import android.util.Log;
 import fdv.d.data.db.Drink;
 
 public class Utils {
 
-    public String getIngregientsList(Drink drink) {
+    public static String getIngregientsList(Drink drink) {
         String s = new String();
+        s = s + drink.getStrIngredient1() + "   " + drink.getStrMeasure1() + "\n";
+        Log.d("TAG"," Ing:" + s);
+        if(drink.getStrIngredient2().isEmpty()) return s;
+        s = s + drink.getStrIngredient2() + "   " + drink.getStrMeasure2() + "\n";
+        if(drink.getStrIngredient3().isEmpty()) return s;
+        s = s + drink.getStrIngredient3() + "   " + drink.getStrMeasure3() + "\n";
+        if(drink.getStrIngredient4().isEmpty()) return s;
+        s = s + drink.getStrIngredient4() + "   " + drink.getStrMeasure4() + "\n";
+        if(drink.getStrIngredient5().isEmpty()) return s;
+        s = s + drink.getStrIngredient5() + "   " + drink.getStrMeasure5() + "\n";
+        if(drink.getStrIngredient6().isEmpty()) return s;
+        s = s + drink.getStrIngredient6() + "   " + drink.getStrMeasure6() + "\n";
+        if(drink.getStrIngredient7().isEmpty()) return s;
+        s = s + drink.getStrIngredient7() + "   " + drink.getStrMeasure7() + "\n";
+        if(drink.getStrIngredient8().isEmpty()) return s;
+        s = s + drink.getStrIngredient8() + "   " + drink.getStrMeasure8() + "\n";
+        if(drink.getStrIngredient9().isEmpty()) return s;
+        s = s + drink.getStrIngredient9() + "   " + drink.getStrMeasure9() + "\n";
+        if(drink.getStrIngredient10().isEmpty()) return s;
+        s = s + drink.getStrIngredient10() + "   " + drink.getStrMeasure10() + "\n";
+        if(drink.getStrIngredient11().isEmpty()) return s;
+        s = s + drink.getStrIngredient11() + "   " + drink.getStrMeasure11() + "\n";
+        if(drink.getStrIngredient12().isEmpty()) return s;
+        s = s + drink.getStrIngredient12() + "   " + drink.getStrMeasure12() + "\n";
+        if(drink.getStrIngredient13().isEmpty()) return s;
+        s = s + drink.getStrIngredient13() + "   " + drink.getStrMeasure13() + "\n";
+        if(drink.getStrIngredient14().isEmpty()) return s;
+        s = s + drink.getStrIngredient14() + "   " + drink.getStrMeasure14() + "\n";
+        if(drink.getStrIngredient15().isEmpty()) return s;
+        s = s + drink.getStrIngredient15() + "   " + drink.getStrMeasure15() + "\n";
     return s;
     }
 
